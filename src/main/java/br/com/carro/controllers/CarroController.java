f0ae22b3efb43c0e797280305dcb835ba5a69bec
Fábio de Alencar Rocha <otymus27@gmail.com>
2025-08-13 21:14:41 -0300

    CRUD Basico funcionando

--- src/main/java/br/com/carro/controllers/CarroController.java
+++ src/main/java/br/com/carro/controllers/CarroController.java
@@ -4,12 +4,12 @@ import br.com.carro.entities.Carro;
 import br.com.carro.services.CarroService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @RestController
@@ -26,10 +26,60 @@ public class CarroController {
     }
 
     @GetMapping
-
     public ResponseEntity<List<Carro>> listar() {
-        List<Carro> lista = carroService.listar();
-        return ResponseEntity.ok(lista);
+        try {
+            List<Carro> lista = this.carroService.listar();
+            return new ResponseEntity<>(lista, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
+
+    // Buscar carro por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
+        try {
+            // Chama o service que retorna o carro ou lança exceção se não existir
+            Carro carro = carroService.buscarPorId(id);
+            return new ResponseEntity<>(carro, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping(   )
+    public ResponseEntity<String> cadastrar(@RequestBody Carro carro) {
+        try {
+            String mensagem = this.carroService.cadastrar(carro);
+            return new ResponseEntity<>(mensagem, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao cadastrar registro: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Atualizar um carro
+    @PutMapping("/{id}")
+    public ResponseEntity<String> atualizar(@PathVariable Integer id, @RequestBody Carro carro) {
+        try {
+            // Atualiza o carro usando o service; se não existir, lança exceção
+            String mensagem = this.carroService.atualizar(id, carro);
+            return new ResponseEntity<>(mensagem, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao atualizar registro: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Excluir um carro
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> excluir(@PathVariable Integer id) {
+        try {
+            // Chama o service que já verifica se o carro existe e lança exceção se não existir
+            String mensagem = this.carroService.excluir(id);
+            return new ResponseEntity<>(mensagem, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao excluir registro: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
 }
--- src/main/java/br/com/carro/services/CarroService.java
+++ src/main/java/br/com/carro/services/CarroService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarroService {
@@ -16,9 +17,36 @@ public class CarroService {
         this.carroRepository = carroRepository;
     }
 
+    // Cadastrar um novo carro diretamente com a entidade sem DTO's
+    public String cadastrar(Carro carro) {
+        // Salva o carro no banco de dados
+        this.carroRepository.save(carro);
+        return "Cadastro feito com sucesso!";
+    }
+
     // Listar todos os carros
     public List<Carro> listar() {
-        return carroRepository.findAll();
+        List<Carro> lista = carroRepository.findAll();
+        return lista;
+    }
+
+    // Buscar carro por ID
+    public Carro buscarPorId(Integer id) throws Exception {
+        Carro carro = this.carroRepository.findById(id).get();
+        return carro;
+    }
+
+    // Atualizar um carro
+    public String atualizar(Integer id, Carro carro) throws Exception {
+        carro.setId(id);
+        this.carroRepository.save(carro);
+        return "Atualização feita com sucesso!";
+    }
+
+    // Excluir um carro
+    public String excluir(Integer id) throws Exception {
+        this.carroRepository.deleteById(id);
+        return "Exclusão feita com sucesso!";
     }
 
 }
